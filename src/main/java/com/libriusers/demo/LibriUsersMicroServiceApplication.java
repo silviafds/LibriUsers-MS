cf74e2ad84b0c1fe89c412c193644a936b689c82
silvi <silviagfernandes1@gmail.com>
2026-01-05 14:16:18 -0300

    feat: config banco de dados

---
+++ src/main/java/com/libriusers/demo/LibriUsersMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.libriusers.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibriUsersMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibriUsersMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libriusers/demo/LibriUsersMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libriusers.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibriUsersMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
